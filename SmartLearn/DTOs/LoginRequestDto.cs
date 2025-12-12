b2418b98600c673c0a3e4f71489a4dfbc1bafab1
tendsT0Zero <nayeemofficial80@gmail.com>
2025-12-13 00:08:04 +0800

    Regis and Login related DTOs are added.

---
+++ SmartLearn/DTOs/LoginRequestDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartLearn.DTOs
+{
+    public class LoginRequestDto
+    {
+        [Required] public string Email { get; set; }
+        [Required] public string Password { get; set; }
+    }
+}
---
+++ SmartLearn/DTOs/RegisterRequestDto.cs
@@ -0,0 +1,25 @@
+﻿using SmartLearn.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace SmartLearn.DTOs
+{
+    public class RegisterRequestDto
+    {
+       
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [JsonIgnore]
+        public string PasswordHash { get; set; }
+
+        [Required]
+        public string Role { get; set; }
+    }
+}
