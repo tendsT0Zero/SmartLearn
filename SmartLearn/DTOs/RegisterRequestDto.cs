45effadfef508214035141a685762f9bbf5aaafa
tendsT0Zero <nayeemofficial80@gmail.com>
2025-12-13 01:48:44 +0800

    PasswordHash changed to Password.

--- SmartLearn/DTOs/RegisterRequestDto.cs
+++ SmartLearn/DTOs/RegisterRequestDto.cs
@@ -16,8 +16,7 @@ namespace SmartLearn.DTOs
         public string Email { get; set; }
 
         [Required]
-        [JsonIgnore]
-        public string PasswordHash { get; set; }
+        public string Password { get; set; }
 
         [Required]
         public string Role { get; set; }
