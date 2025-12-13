10ab8f36c0310d6cdda6b55ee2940a88c2797cc2
tendsT0Zero <nayeemofficial80@gmail.com>
2025-12-13 21:48:12 +0800

    API end points are established related to Course Functionalities.

---
+++ SmartLearn/Controllers/CourseController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SmartLearn.DTOs.CourseDTO;
+using SmartLearn.Services;
+using System.Security.Claims;
+
+namespace SmartLearn.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        private readonly ICourseService _courseService;
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCourses()
+        {
+            var courses = await _courseService.GetAllCoursesAsync();
+
+            return Ok(courses);
+        }
+        [Authorize(Roles = "Instructor, Admin")]
+        [HttpPost]
+        public async Task<IActionResult> CreateCourse([FromForm] CreateCourseDTO dto)
+        {
+            // Fetching Current User's ID from the JWT Token
+            var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier);
+            if (userIdClaim == null) return Unauthorized("Unauthorized user.");
+
+            int instructorId = int.Parse(userIdClaim.Value);
+            var course = await _courseService.CreateCourseAsync(dto, instructorId);
+
+            return Ok(new { Message = "Course created!", CourseId = course.Id, ImageUrl = course.ImageUrl });
+        }
+    }
+}
