345db026c1d8808a8a2fc8060dd9d19516308287
tendsT0Zero <nayeemofficial80@gmail.com>
2025-12-16 01:05:19 +0800

    DTO for updating a course.

---
+++ SmartLearn/DTOs/CourseDTO/UpdateCourseDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace SmartLearn.DTOs.CourseDTO
+{
+    public class UpdateCourseDTO
+    {
+        public string Title { get; set; } = null!;
+        public string Description { get; set; }
+        public int CategoryId { get; set; }
+        public decimal Price { get; set; }
+        public IFormFile? CoverImage { get; set; }
+    }
+}
